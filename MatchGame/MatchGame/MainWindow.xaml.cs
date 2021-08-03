d736f02caa08d65c0e1a29a464e61e523605dd66
P <psyud@pm.me>
2021-08-03 20:49:50 +0700

    MatchGame UI

---
+++ MatchGame/MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            var emojis = new List<string>
+            {
+                "🐕", "🐕",
+                "🐎", "🐎",
+                "🐄", "🐄",
+                "🐐", "🐐",
+                "🐀", "🐀",
+                "🦔", "🦔",
+                "🦘", "🦘",
+                "🦨", "🦨"
+            };
+
+            var random = new Random();
+            foreach(var textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(emojis.Count);
+                textBlock.Text = emojis[index];
+                emojis.RemoveAt(index);
+            }
+        }
+    }
+}
