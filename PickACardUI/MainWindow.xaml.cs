e23a4da142b204afd891587ee4eab14ead073a88
P <psyud@pm.me>
2021-08-10 10:34:23 +0700

    pick a card

---
+++ PickACardUI/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace PickACardUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] pickedCards = CardPicker.PickSomeCards((int)numberOfCards.Value);
+            listOfCards.Items.Clear();
+            foreach (string card in pickedCards)
+            {
+                listOfCards.Items.Add(card);
+            }
+        }
+    }
+}
